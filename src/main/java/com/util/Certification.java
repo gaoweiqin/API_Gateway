ed0e1519abc0d59ba110c83273d60514cf6fb44f
gaowq <Mr.gaoweiqin@outlook.com>
2018-08-08 09:46:56 +0800

    secret2

--- src/main/java/com/util/Certification.java
+++ src/main/java/com/util/Certification.java
@@ -1,31 +1,35 @@
 package com.util;
+
 import java.util.Date;
 import java.text.SimpleDateFormat;
+
 public class Certification {
     public static void main(String[] args) {
         //24小时 = 86400000毫秒，比较时间戳的差
         getTime("1533634450582");
         //获取加密后的文件
-        try{
-            String strA=  BasicAuthenticationUtil.hmacSha1Encrypt("123456","1533634450582");
+        try {
+            String strB = BasicAuthenticationUtil.hmacSha1Encrypt("123456", "1533634450582");
+            System.out.println(strB);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+
     /**
      * 获取当前系统时间
      */
-    public static void getTime(String Date){
+    public static void getTime(String Date) {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
         //比较Date和 df
         System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
-        long timenum=System.currentTimeMillis();
+        long timenum = System.currentTimeMillis();
 
-        Long now=System.currentTimeMillis();
+        Long now = System.currentTimeMillis();
         System.out.println(now);
         //24小时 = 86400000毫秒
-        long t=now-Long.parseLong(Date);
+        long t = now - Long.parseLong(Date);
         System.out.println(t);
 
     }
